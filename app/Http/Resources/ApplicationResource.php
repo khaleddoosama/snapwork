c49991ea040405c165de8bd2ade485f65c06589d
khaleddoosama <khaleddoosama@gmail.com>
2024-05-06 11:45:13 +0300

    edit in application resource

--- app/Http/Resources/ApplicationResource.php
+++ app/Http/Resources/ApplicationResource.php
@@ -24,6 +24,26 @@ class ApplicationResource extends JsonResource
             'attachments' => $this->attachments,
             'created_at' => $this->created_at,
             'status' => $this->status,
+            'job' => [
+                'id' => $this->job->id,
+                'slug' => $this->job->slug,
+                'client' => new UserResource($this->job->client),
+                'specialization' => new SpecializationResource($this->job->specialization),
+                'title' => $this->job->title,
+                'description' => $this->job->description,
+                'required_skills' => $this->job->required_skills,
+                'expected_budget' => $this->job->expected_budget,
+                'expected_duration' => $this->job->expected_duration,
+                'attachments' => $this->job->attachments,
+                'created_at' => $this->job->created_at,
+                'status' => $this->job->status,
+                'type' => $this->job->type,
+                'location_type' => $this->job->location_type,
+                'longitude' => $this->job->longitude,
+                'latitude' => $this->job->latitude,
+                'address' => $this->job->address,
+                'reqeusts' => RequestChangeResource::collection($this->job->requestChanges),
+            ],
         ];
     }
 }
