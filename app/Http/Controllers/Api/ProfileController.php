5f3e2bb32f7120b4169b5dd1939777a17854518f
khaleddoosama <khaleddoosama@gmail.com>
2024-05-14 10:31:43 +0300

    improvments in profile controller

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -11,10 +11,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Cache;
 
 class ProfileController extends Controller
 {
@@ -38,8 +37,8 @@ class ProfileController extends Controller
         }
 
         $user = Auth::user();
-        $user->password = bcrypt($request->password);
-        $user->save();
+        $user->update(['password' => bcrypt($request->password)]);
+
 
         return $this->apiResponse(new UserResource($user), 'Password changed successfully', 200);
     }
@@ -59,24 +58,23 @@ class ProfileController extends Controller
 
             $user = Auth::user();
             $skills = $request->skills;
-            $user_skills = $user->skills()->pluck('name')->toArray();
+            $existingSkills = $user->skills()->pluck('name')->toArray();
 
-            $diff_skills = array_diff($skills, $user_skills);
+            $skillsToAdd = array_diff($skills, $existingSkills);
+            $skillsToRemove = array_diff($existingSkills, $skills);
 
-            // remove unselected skill
-            $user->skills()->detach($user->skills->whereNotIn('name', $skills)->pluck('id')->toArray());
-
-            if (count($diff_skills) > 0) {
-                foreach ($diff_skills as $skill) {
-                    $skill_exist = Skill::where('name', $skill)->first();
-
-                    if (!$skill_exist) {
-                        $skill_exist = Skill::create(['name' => $skill]);
-                    }
-
-                    $user->skills()->attach($skill_exist);
+            DB::transaction(function () use ($user, $skillsToAdd, $skillsToRemove) {
+                if ($skillsToRemove) {
+                    $user->skills()->detach(Skill::whereIn('name', $skillsToRemove)->pluck('id'));
                 }
-            }
+
+                if ($skillsToAdd) {
+                    foreach ($skillsToAdd as $skillName) {
+                        $skill = Skill::firstOrCreate(['name' => $skillName]);
+                        $user->skills()->attach($skill);
+                    }
+                }
+            });
 
             return $this->apiResponse(new UserResource($user), 'Skills updated successfully', 200);
         } catch (Exception $e) {
@@ -84,209 +82,110 @@ class ProfileController extends Controller
         }
     }
 
-    // update languages
+    private function updateUserData($request, $rules, $relation, $resourceMessage)
+    {
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return $this->apiResponse(null, $validator->errors()->first(), 422);
+        }
+
+        $user = Auth::user();
+        $data = $request->all();
+
+        DB::transaction(function () use ($user, $data, $relation, &$ids) {
+            $ids = [];
+            foreach ($data[$relation] as $item) {
+
+                $model = $user->$relation()->updateOrCreate(
+                    ['id' => $item['id'] ?? null],
+                    $item
+                );
+                $ids[] = $model->id;
+            }
+
+            $user->$relation()->whereNotIn('id', $ids)->delete();
+        });
+
+        return $this->apiResponse(new UserResource($user), $resourceMessage, 200);
+    }
+
     public function updateLanguages(Request $request)
     {
-        try {
-            $validator = Validator::make($request->all(), [
-                'languages' => 'required|array',
-                'languages.*.name' => 'required|string',
-                'languages.*.level' => 'required',
-            ]);
+        $rules = [
+            'languages' => 'required|array',
+            'languages.*.name' => 'required|string',
+            'languages.*.level' => 'required',
+        ];
 
-            if ($validator->fails()) {
-                return $this->apiResponse(null, $validator->errors()->first(), 422);
-            }
-
-            $user = Auth::user();
-
-            // update existing languages or create new ones
-            foreach ($request->languages as $languageData) {
-                $language = $user->languages()->updateOrCreate(
-                    ['id' => $languageData['id'] ?? null], // Assuming 'id' is a unique identifier for languages
-                    $languageData
-                );
-                $arr[] = $language->id;
-            }
-
-            // remove old languages
-            $user->languages()->whereNotIn('id', $arr)->delete();
-
-            return $this->apiResponse(new UserResource($user), 'Languages updated successfully', 200);
-        } catch (Exception $e) {
-            return $this->apiResponse(null, $e->getMessage(), 500);
-        }
+        return $this->updateUserData($request, $rules, 'languages', 'Languages updated successfully');
     }
 
-    // update educations
     public function updateEducations(Request $request)
     {
-        try {
-            $validator = Validator::make($request->all(), [
-                'educations' => 'required|array',
-                'educations.*.school' => 'required|string',
-                'educations.*.degree' => 'required|string',
-                'educations.*.start_date' => 'required|date',
-                'educations.*.end_date' => 'required|date',
-                'educations.*.major' => 'nullable|string',
-                'educations.*.description' => 'nullable|string',
-            ]);
+        $rules = [
+            'educations' => 'required|array',
+            'educations.*.school' => 'required|string',
+            'educations.*.degree' => 'required|string',
+            'educations.*.start_date' => 'required|date',
+            'educations.*.end_date' => 'required|date',
+            'educations.*.major' => 'nullable|string',
+            'educations.*.description' => 'nullable|string',
+        ];
 
-            if ($validator->fails()) {
-                return $this->apiResponse(null, $validator->errors()->first(), 422);
-            }
-
-            $user = Auth::user();
-
-            // Update existing educations or create new ones
-            foreach ($request->educations as $educationData) {
-                $education = $user->educations()->updateOrCreate(
-                    ['id' => $educationData['id'] ?? null], // Assuming 'id' is a unique identifier for educations
-                    $educationData
-                );
-
-                $arr[] = $education->id;
-            }
-
-            // Remove any educations that are not in the new list
-            $user->educations()->whereNotIn('id', $arr)->delete();
-
-            return $this->apiResponse(new UserResource($user), 'Educations updated successfully', 200);
-        } catch (Exception $e) {
-            return $this->apiResponse(null, $e->getMessage(), 500);
-        }
+        return $this->updateUserData($request, $rules, 'educations', 'Educations updated successfully');
     }
 
-    //employments
     public function updateEmployments(Request $request)
     {
-        try {
-            $validator = Validator::make($request->all(), [
-                'employments' => 'required|array',
-                'employments.*.company' => 'required|string',
-                'employments.*.position' => 'required|string',
-                'employments.*.city' => 'nullable|string',
-                'employments.*.country' => 'nullable|string',
-                'employments.*.start_date' => 'required|date',
-                'employments.*.end_date' => 'required|date',
-                'employments.*.description' => 'nullable|string',
-            ]);
+        $rules = [
+            'employments' => 'required|array',
+            'employments.*.company' => 'required|string',
+            'employments.*.position' => 'required|string',
+            'employments.*.city' => 'nullable|string',
+            'employments.*.country' => 'nullable|string',
+            'employments.*.start_date' => 'required|date',
+            'employments.*.end_date' => 'required|date',
+            'employments.*.description' => 'nullable|string',
+        ];
 
-            if ($validator->fails()) {
-                return $this->apiResponse(null, $validator->errors()->first(), 422);
-            }
-
-            $user = Auth::user();
-
-            $arr = [];
-            // Update existing projects or create new ones
-            foreach ($request->employments as $employmentData) {
-                $employment = $user->employments()->updateOrCreate(
-                    ['id' => $employmentData['id'] ?? null], // Assuming 'id' is a unique identifier for employments
-                    $employmentData
-                );
-                $arr[] = $employment->id;
-            }
-
-            // Remove any projects that are not in the new list
-            $user->employments()->whereNotIn('id', $arr)->delete();
-
-            return $this->apiResponse(new UserResource($user), 'Employments updated successfully', 200);
-        } catch (Exception $e) {
-            return $this->apiResponse(null, $e->getMessage(), 500);
-        } catch (Exception $e) {
-            return $this->apiResponse(null, $e->getMessage(), 500);
-        }
+        return $this->updateUserData($request, $rules, 'employments', 'Employments updated successfully');
     }
 
-    // update projects
-    // public function updateProjects(Request $request)
-    // {
-
-    //     try {
-    //         $validator = Validator::make($request->all(), [
-    //             'projects' => 'required|array',
-    //             'projects.*.title' => 'required|string',
-    //             'projects.*.description' => 'required|string',
-    //             'projects.*.url' => 'nullable|url',
-    //             'projects.*.technologies' => 'nullable|array',
-    //             'projects.*.completion_date' => 'nullable|date',
-    //             'projects.*.thumbnail' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    //             'projects.*.attachments' => 'nullable|array',
-    //         ]);
-    //         if ($validator->fails()) {
-    //             return $this->apiResponse(null, $validator->errors()->first(), 422);
-    //         }
-
-    //         $user = Auth::user();
-
-    //         // Update existing projects or create new ones
-    //         foreach ($request->projects as $projectData) {
-    //             $project = $user->projects()->updateOrCreate(
-    //                 ['id' => $projectData['id'] ?? null], // Assuming 'id' is a unique identifier for projects
-    //                 $projectData
-    //             );
-    //         }
-
-    //         return $this->apiResponse(new UserResource($user), 'Projects updated successfully', 200);
-    //     } catch (Exception $e) {
-    //         return $this->apiResponse(null, $e->getMessage(), 500);
-    //     }
-    // }
-
-    // update certifications
     public function updateCertifications(Request $request)
     {
-        try {
-            $validator = Validator::make($request->all(), [
-                'certifications' => 'required|array',
-                'certifications.*.name' => 'required|string',
-                'certifications.*.issuer' => 'required|string',
-                'certifications.*.issue_date' => 'required|date',
-                'certifications.*.url' => 'nullable|url',
-                'certifications.*.description' => 'nullable|string',
-            ]);
+        $rules = [
+            'certifications' => 'required|array',
+            'certifications.*.name' => 'required|string',
+            'certifications.*.issuer' => 'required|string',
+            'certifications.*.issue_date' => 'required|date',
+            'certifications.*.url' => 'nullable|url',
+            'certifications.*.description' => 'nullable|string',
+        ];
 
-            if ($validator->fails()) {
-                return $this->apiResponse(null, $validator->errors()->first(), 422);
-            }
-
-            $user = Auth::user();
-
-            // Update existing certifications or create new ones
-            foreach ($request->certifications as $certificationData) {
-                $certification = $user->certifications()->updateOrCreate(
-                    ['id' => $certificationData['id'] ?? null], // Assuming 'id' is a unique identifier for certifications
-                    $certificationData
-                );
-
-                $arr[] = $certification->id;
-            }
-
-            // Remove any certifications that are not in the new list
-            $user->certifications()->whereNotIn('id', $arr)->delete();
-
-            return $this->apiResponse(new UserResource($user), 'Certifications updated successfully', 200);
-        } catch (Exception $e) {
-            return $this->apiResponse(null, $e->getMessage(), 500);
-        }
+        return $this->updateUserData($request, $rules, 'certifications', 'Certifications updated successfully');
     }
+
 
     public function getSpecializations()
     {
-        $specializations = Specialization::get();
+        $specializations = Cache::remember('specializations', 60, function () {
+            return Specialization::all();
+        });
         return $this->apiResponse(SpecializationResource::collection($specializations), 'Specializations fetched successfully', 200);
     }
 
     //getFreelancers
     public function getFreelancers($specialization_id = null)
     {
-        if ($specialization_id) {
-            // return users with scope freelancer (scopeFreelancer) and return last 10
-            $freelancers = User::freelancer()->where('specialization_id', $specialization_id)->latest()->take(10)->get();
-        } else {
-            $freelancers = User::freelancer()->latest()->take(10)->get();
-        }
+        $freelancers = User::freelancer()
+            ->when($specialization_id, function ($query) use ($specialization_id) {
+                return $query->where('specialization_id', $specialization_id);
+            })
+            ->latest()
+            ->take(10)
+            ->get();
+
         return $this->apiResponse(UserResource::collection($freelancers), 'Freelancers fetched successfully', 200);
     }
 }
